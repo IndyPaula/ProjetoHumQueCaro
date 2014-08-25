3396bef15c2f7395b45f1b51f41bbabcee97c7db
Luiz Antonio <luiz.antonioPS95@gmail.com>
2014-08-25 09:09:48 -0300

    Criação dos getters e setters do bean.

--- ProjetoHumQueCaro/src/ifpb/monteiro/ads/pd/beans/Produto.java
+++ ProjetoHumQueCaro/src/ifpb/monteiro/ads/pd/beans/Produto.java
@@ -1,5 +1,33 @@
 package ifpb.monteiro.ads.pd.beans;
 
 public class Produto {
-
+	
+	String nome;
+	String codigo;
+	String fabricante;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getFabricante() {
+		return fabricante;
+	}
+	public void setFabricante(String fabricante) {
+		this.fabricante = fabricante;
+	}
+	public String getCodigo() {
+		return codigo;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == null || obj.getClass() != this.getClass()) {
+			return false;
+		}
+		Produto produto = (Produto) obj;
+		return produto.getCodigo().equals(getCodigo());
+	}
 }
