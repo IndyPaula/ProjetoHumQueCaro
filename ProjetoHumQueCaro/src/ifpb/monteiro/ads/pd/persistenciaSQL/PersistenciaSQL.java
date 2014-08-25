35f54eea4afd6112bde9c71de2400b242210eb26
Joaquim Maia <joaquim.cmh@gmail.com>
2014-08-25 14:13:44 -0300

    Persistencia Sql (lembrar de modificar para sua senha padrao)

--- ProjetoHumQueCaro/src/ifpb/monteiro/ads/pd/persistenciaSQL/PersistenciaSQL.java
+++ ProjetoHumQueCaro/src/ifpb/monteiro/ads/pd/persistenciaSQL/PersistenciaSQL.java
@@ -1,5 +1,18 @@
 package ifpb.monteiro.ads.pd.persistenciaSQL;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class PersistenciaSQL {
+
+	public static Connection conectar() throws SQLException {
+		String url = "jdbc:mysql://127.0.0.1/HumQueCaroProjeto";
+		String username = "root";
+		String password = "senhafraca";
+		Connection connection;
+		connection = DriverManager.getConnection(url, username, password);
+		return connection;
+	}
 
 }
