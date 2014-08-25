ad80bdac9366d2f0c0c10eb3c5d84f961c37d596
Deivid Azevedo <deividazvedo1@gmail.com>
2014-08-25 03:28:02 -0300

    Classe para os testes JUnit

---
+++ ProjetoHumQueCaro/src/ifpb/monteiro/ads/pd/test/TestProjetoHumQueCaro.java
@@ -0,0 +1,14 @@
+package ifpb.monteiro.ads.pd.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestProjetoHumQueCaro {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
