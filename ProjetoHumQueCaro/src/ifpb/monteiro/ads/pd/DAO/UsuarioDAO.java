aa274c5dd23b43c4cd7ad17e57686738cc537f35
Joaquim Maia <joaquim.cmh@gmail.com>
2014-08-25 17:47:01 -0300

    Salvando arquivos no Banco

--- ProjetoHumQueCaro/src/ifpb/monteiro/ads/pd/DAO/UsuarioDAO.java
+++ ProjetoHumQueCaro/src/ifpb/monteiro/ads/pd/DAO/UsuarioDAO.java
@@ -1,5 +1,6 @@
 package ifpb.monteiro.ads.pd.DAO;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import ifpb.monteiro.ads.pd.beans.Usuario;
@@ -25,20 +26,41 @@ public class UsuarioDAO extends DAO<Usuario> {
 
 	@Override
 	public void remove(Usuario algo) throws HumQueCaroException {
-		// TODO Auto-generated method stub
-
+		try {
+			abrirBanco();
+			Usuario userAux = procura(algo.getEmail());
+			getStmt().executeUpdate("DELETE FROM usuarios WHERE codigo_usuario ='"+userAux.getCodigoUsuario()+"'");
+			fecharBanco();
+		} catch (Exception e) {
+			throw new HumQueCaroException("- Erro ao remover Usuario -"+e.getMessage());
+		}
+		
 	}
 
 	@Override
 	public void altera(Usuario algo) throws HumQueCaroException {
-		// TODO Auto-generated method stub
+		// TODO Depois arrumar isso x.x
 
 	}
 
 	@Override
 	public Usuario procura(String algo) throws HumQueCaroException {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			abrirBanco();
+			Usuario pessoa = null;
+			ResultSet rs;
+			rs = getStmt().executeQuery(
+					"SELECT * FROM usuarios WHERE usuarios.email like '"
+							+ algo + "'");
+			while (rs.next()) {
+				pessoa = new Usuario(rs.getString("email"), rs.getString("senha"), rs.getString("nome"), rs.getInt("codigo_usuario"));
+			}
+			return pessoa;
+
+		} catch (Exception e) {
+			throw new HumQueCaroException(" Erro ao procurar uma Usuario - "
+					+ e.getMessage());
+		}
 	}
 
 }
