ee0415959ea0ddc995e249191fef322ecc03330c
Joaquim Maia <joaquim.cmh@gmail.com>
2014-08-26 14:24:47 -0300

    Validacao de nome e email

--- ProjetoHumQueCaro/src/ifpb/monteiro/ads/pd/validacao/Validacao.java
+++ ProjetoHumQueCaro/src/ifpb/monteiro/ads/pd/validacao/Validacao.java
@@ -9,5 +9,48 @@ public class Validacao {
 			throw new HumQueCaroException(message);
 		}
 	}
+	
+	/**
+	 * Metodo criado para verificar se o email foi digitado corretamente. Sera
+	 * verificado a quantidade de @ e se contem algum nome valido antes dela.
+	 * 
+	 * @param email
+	 *            que vai ser verificado
+	 * @param msg
+	 * 
+	 * @throws BabyCareException
+	 *             Caso o email seja invalido sera lançada a exeption atributo
+	 *             invalido
+	 */
+	private static void verNomeEmail(String email, String msg)
+			throws HumQueCaroException {
+		if (email != null && contRept(email, '@') == 1) {
+			String[] mail = email.split("@");
+			if (mail[0].length() == 0) {
+				throw new HumQueCaroException(msg);
+			}
+		}
+	}
+
+	/**
+	 * Este metodo eh usado para contar a repeticao de um elemento em uma
+	 * String.
+	 * 
+	 * @param elemento
+	 *            item que sera comparado
+	 * @return retorna a quantidade de vezes que ele aparece na String
+	 */
+
+	public static int contRept(String frs, char elem) {
+		int repetiu = 0;
+		if (frs != null) {
+			for (char elemento : frs.toCharArray()) {
+				if (elemento == elem) {
+					repetiu++;
+				}
+			}
+		}
+		return repetiu;
+	}
 
 }
