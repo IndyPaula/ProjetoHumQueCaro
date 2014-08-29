10b4fa8afa1863b062f6b77e5e03b3a7ebc295de
Indy Paula <indypaula12@gmail.com>
2014-08-29 19:24:08 -0300

    O método altera do usuarioDAO esta alterando no Banco normalmente!!

--- ProjetoHumQueCaro/src/ifpb/monteiro/ads/pd/DAO/UsuarioDAO.java
+++ ProjetoHumQueCaro/src/ifpb/monteiro/ads/pd/DAO/UsuarioDAO.java
@@ -1,5 +1,6 @@
 package ifpb.monteiro.ads.pd.DAO;
 
+
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
@@ -29,17 +30,37 @@ public class UsuarioDAO extends DAO<Usuario> {
 		try {
 			abrirBanco();
 			Usuario userAux = procura(algo.getEmail());
-			getStmt().executeUpdate("DELETE FROM usuarios WHERE codigo_usuario ='"+userAux.getCodigoUsuario()+"'");
+			getStmt().executeUpdate(
+					"DELETE FROM usuarios WHERE codigo_usuario ='"
+							+ userAux.getCodigoUsuario() + "'");
 			fecharBanco();
 		} catch (Exception e) {
-			throw new HumQueCaroException("- Erro ao remover Usuario -"+e.getMessage());
+			throw new HumQueCaroException("- Erro ao remover Usuario -"
+					+ e.getMessage());
 		}
-		
+
 	}
 
 	@Override
-	public void altera(Usuario algo) throws HumQueCaroException {
-		// TODO Depois arrumar isso x.x
+	public void altera(Usuario usuario) throws HumQueCaroException {
+		try {
+			abrirBanco();
+
+			getStmt().executeUpdate(
+					"UPDATE usuarios SET codigo_usuario = '"
+							+ usuario.getCodigoUsuario()
+							+ "', email = '" + usuario.getEmail()
+							+ "', nome ='" + usuario.getNome()
+							+ "', senha = '" + usuario.getSenha()
+							+ "' WHERE codigo_usuario like '" + usuario.getCodigoUsuario()
+							+ "'");
+
+			fecharBanco();
+
+		} catch (SQLException exception) {
+			throw new HumQueCaroException("Usuario nao foi alterado!"
+					+ exception.getMessage());
+		}
 
 	}
 
@@ -50,10 +71,12 @@ public class UsuarioDAO extends DAO<Usuario> {
 			Usuario pessoa = null;
 			ResultSet rs;
 			rs = getStmt().executeQuery(
-					"SELECT * FROM usuarios WHERE usuarios.email like '"
-							+ algo + "'");
+					"SELECT * FROM usuarios WHERE usuarios.email like '" + algo
+							+ "'");
 			while (rs.next()) {
-				pessoa = new Usuario(rs.getString("email"), rs.getString("senha"), rs.getString("nome"), rs.getInt("codigo_usuario"));
+				pessoa = new Usuario(rs.getString("email"),
+						rs.getString("senha"), rs.getString("nome"),
+						rs.getInt("codigo_usuario"));
 			}
 			return pessoa;
 
